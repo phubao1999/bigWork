e1450d510706e3789435e6a6bb1687121b7011eb
phubao1999 <phutuongbao1999@gmail.com>
2019-05-21 16:41:41 +0700

    7/14

--- elsa/elsa.js
+++ elsa/elsa.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    
+    $(window).scroll(function () { 
+        if($('html, body').scrollTop()>430){
+            $('.menutren').addClass('abc');
+        }
+        else if($('html, body').scrollTop()<=100){
+            $('.menutren').removeClass('abc');
+        }
+    });
 });
