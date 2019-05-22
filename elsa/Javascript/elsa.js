4a7c827be29fb8e9db25144a2a11751dc91e0664
phubao1999 <phutuongbao1999@gmail.com>
2019-05-23 02:35:50 +0700

    Update Google Map

--- elsa/Javascript/elsa.js
+++ elsa/Javascript/elsa.js
@@ -7,4 +7,15 @@ $(document).ready(function () {
             $('.menutren').removeClass('abc');
         }
     });
+
+    function initialize (){
+        var mapOptions = {
+            center: new google.maps.LatLng(16.048855, 108.241467),
+            zoom: 8
+        };
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }
 });
