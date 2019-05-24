adec929f1eb5243d489a518137662e1386ad80fc
phubao1999 <phutuongbao1999@gmail.com>
2019-05-25 01:28:14 +0700

    Deploy to Fire Base

---
+++ SanFox/indexcompany/public/Javascript/filter.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Hiệu Ứng Filter
+    
+    $('.ourProject ul li').click(function (e) { 
+        e.preventDefault();
+        var danhMuc = $(this).data('class');
+        // console.log(danhMuc);
+        $('.effect img').each(function(index, el){
+            if($(this).hasClass(danhMuc)){
+                $(this).fadeIn();
+            }
+            else {
+                $(this).fadeOut();
+            }
+        });
+    });
+});
