4b84342e9a7eb10b570d168f6d688123d9dc7638
phubao1999 <phutuongbao1999@gmail.com>
2019-05-31 00:11:20 +0700

    Finish index of ke toan

--- SanFox/Index/ketoan/Javascript/index.js
+++ SanFox/Index/ketoan/Javascript/index.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
     $('.nutmenu i.fas.fa-bars.d-none').click(function (e) { 
         e.preventDefault();
-        console.log('aaa');
         $('.ndmenu').toggleClass('rightMove');
+    });
+    $(window).scroll(function(){
+        if($('html, body').scrollTop()>430){
+            $('.menu').addClass('bg-lightaaa');
+            $('.menu ul li a').addClass('chuDen');
+            $('.menu .logo img').addClass('nhoLai');
+            $('.nutlentren').addClass('backToTop');
+        }
+        else if($('html, body').scrollTop()<= 100){
+            $('.menu').removeClass('bg-lightaaa');
+            $('.menu ul li a').removeClass('chuDen');
+            $('.menu .logo img').removeClass('nhoLai');
+            $('.nutlentren').removeClass('backToTop');
+        } 
+    });
+    $('.nutlentren').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({'scrollTop':0}, 1000);
     });
 });
