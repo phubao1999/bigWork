6c8c532261b5d33892e280a1bc667e714a61a76f
phubao1999 <phutuongbao1999@gmail.com>
2019-06-04 23:22:17 +0700

    Update Some Web Page

--- SanFox/Index/imgTemplate/JavaScript/index.js
+++ SanFox/Index/imgTemplate/JavaScript/index.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
+    new WOW().init();
+});
+$(document).ready(function () {
     // console.log('aaa');
     $('.menu .phai li a').click(function (e) { 
         e.preventDefault();
         $('.menuChild').toggleClass('hienra');
+    });
+    $('.backToTop').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
     });
 });
 
 $(document).ready(function () {
     $('.cacanh').isotope({
         itemSelector: 'li'
+    });
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () { 
+        if($('html, body').scrollTop()>430){
+            $('i.fas.fa-long-arrow-alt-up').addClass('hienra');
+        } else if ($('html, body').scrollTop()<=100){
+            $('i.fas.fa-long-arrow-alt-up').removeClass('hienra');
+        }
     });
 });
