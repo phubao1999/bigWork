46de158c6022947a586618571045d94cc31420c4
phubao1999 <phutuongbao1999@gmail.com>
2019-06-09 10:15:23 +0700

    My New Theme

--- SanFox/Index/Theme/game/Javascript/index.js
+++ SanFox/Index/Theme/game/Javascript/index.js
@@ -33,3 +33,4 @@ $(document).ready(function () {
         $('html, body').animate({ scrollTop:0 },1000);
     });
 });
+
---
+++ SanFox/Index/Theme/hotel/javascript/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+   $('#drop').click(function (e) { 
+       e.preventDefault();
+       $('#drop ul').toggleClass('down');
+   }); 
+   $('.barMenu').click(function (e) { 
+       e.preventDefault();
+       $('.dropdown').toggleClass('showUp');
+   });
+   $(window).scroll(function(){
+       if($('html, body').scrollTop() > 300){
+            $('.backToTop').addClass('show');
+       } else if($('html, body').scrollTop() < 100){
+            $('.backToTop').removeClass('show');
+       }
+   })
+
+   $('.backToTop').click(function (e) { 
+       e.preventDefault();
+       $('html, body').animate({scrollTop:0}, 1000);
+   });
+});
